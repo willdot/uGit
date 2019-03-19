73ff2a1a06fc15530ac09e4ef1059497eba918bd
Will <will7989@hotmail.com>
2019-03-19 18:55:32 +0000

    remove current branch when prompting to check out branch

---
+++ app/pkg/git/branch.go
@@ -0,0 +1,66 @@
+package git
+
+import (
+	"strings"
+	"uGit/app/pkg/run"
+
+	"github.com/pkg/errors"
+)
+
+// ErrNoCurrentBranchFound is an error returned when the input branches doesn't contain a current branch indicator
+var ErrNoCurrentBranchFound = errors.New("no current branch found")
+
+// ErrBranchDoesNotExist is an error for when the branch asked to be checked out, doesn't exist
+var ErrBranchDoesNotExist = errors.New("Cannot checkout branch as it doesn't exist")
+
+// SplitBranches takes a string of branches with newline separators and splits them into a slice
+func SplitBranches(s string) []string {
+	result := strings.Split(s, "\n")
+
+	return result
+}
+
+// GetCurrentBranch takes a slice of branch names and returns the current branch based on which one starts with an *
+func GetCurrentBranch(branches []string) (string, error) {
+	for _, r := range branches {
+		if r == "" {
+			continue
+		}
+
+		if string(r[0]) == "*" {
+			return r, nil
+		}
+	}
+
+	return "", ErrNoCurrentBranchFound
+}
+
+// GetBranches gets all local branches
+func GetBranches(commander run.Commander) (string, error) {
+	result, err := run.CommandWithResult(commander, "git", "branch")
+
+	return result, err
+}
+
+//RemoveCurrentBranch will remove the current branch from a list of branches
+func RemoveCurrentBranch(branches []string) []string {
+
+	var result []string
+	current, _ := GetCurrentBranch(branches)
+
+	for i := 0; i < len(branches); i++ {
+		if branches[i] != current {
+			result = append(result, branches[i])
+		}
+	}
+
+	return result
+}
+
+// CheckoutBranch checks out a branch
+func CheckoutBranch(commander run.Commander, branch string) (string, error) {
+
+	result, err := run.CommandWithResult(commander, "git", "checkout", branch)
+
+	return string(result), err
+}
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"uGit/internal/cmd"
+	root "uGit/app/cmd"
 )
 
 func main() {
 	root.Execute()
-	//fmt.Printf("error: %v", errors.WithMessage(err, ""))
 }

75c1b6395938cc55ca87db624ac84cfb041385f7
Will Andrews <will7989@hotmail.com>
2019-03-19 13:01:54 +0000

    Removed unnecessary code to root command

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -3,31 +3,13 @@ package root
 import (
 	"fmt"
 	"os"
-	"uGit/internal/pkg/git"
-	"uGit/internal/pkg/run"
 
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "uGit",
 	Short: "uGit is an application to run common git commands quicker ",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Starting")
-
-		commander := run.RealCommander{}
-		result, _ := run.CommandWithResult(commander, "git", "branch")
-
-		branches := git.SplitBranches(result)
-		current, err := git.GetCurrentBranch(branches)
-
-		if err != nil {
-			fmt.Printf("error: %v", errors.WithMessage(err, ""))
-		}
-
-		fmt.Println(current)
-	},
 }
 
 // Execute starts the app

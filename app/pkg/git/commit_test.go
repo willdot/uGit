855b832b9c75eea01a6ce524b2ce5c328d321abd
willdot <will7989@hotmail.com>
2019-03-20 13:30:35 +0000

    test commit

--- app/pkg/git/commit_test.go
+++ app/pkg/git/commit_test.go
@@ -6,4 +6,15 @@ import (
 
 func TestCommit(t *testing.T) {
 
+	want := "success"
+
+	fake := &FakeCommander{
+		Result: []byte(want),
+	}
+
+	got, _ := CommitChanges(fake)
+
+	if got != want {
+		t.Errorf("want '%s' but got '%s'", want, got)
+	}
 }

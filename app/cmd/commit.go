79199f9c53bb77ab455e28e24c4ea97566a30395
Will Andrews <will7989@hotmail.com>
2019-03-25 12:14:55 +0000

    refactored the commit file

--- app/cmd/commit.go
+++ app/cmd/commit.go
@@ -17,38 +17,16 @@ var commitCmd = &cobra.Command{
 	Short: "Commit changes",
 	Run: func(cmd *cobra.Command, args []string) {
 
-		untrackedFilesCommander := run.Commander{
-			Command: "git",
-			Args:    []string{"status"},
-		}
+		workOutFilesToBeCommitted()
 
-		status, err := git.Status(untrackedFilesCommander)
+		status := getStatus()
 
-		untrackedFiles, nothingToCommit := git.GetFilesOrNothingToCommit(status)
+		filesToBeCommitted := git.GetFilesToBeCommitted(status)
 
-		if err != nil {
-			fmt.Printf("error: %v", errors.WithMessage(err, ""))
-			os.Exit(1)
-		}
-
-		if nothingToCommit {
+		if len(filesToBeCommitted) == 0 {
 			fmt.Println("Nothing to commit")
 			return
 		}
-
-		if len(untrackedFiles) > 0 {
-			resolveUntrackedFiles(untrackedFiles)
-		}
-
-		notStaged := git.GetNotStagedFiles(status)
-
-		if len(notStaged) > 0 {
-			stageFiles(notStaged)
-		}
-
-		status, err = git.Status(untrackedFilesCommander)
-
-		filesToBeCommitted := git.GetFilesToBeCommitted(status)
 
 		fmt.Println("Files to be committed")
 		for _, file := range filesToBeCommitted {
@@ -59,36 +37,59 @@ var commitCmd = &cobra.Command{
 	},
 }
 
-func resolveUntrackedFiles(untrackedFiles []string) {
-	var selectedFiles []string
+func workOutFilesToBeCommitted() {
+	status := getStatus()
 
-	selectedFiles = selectFilesToTrack(untrackedFiles)
+	untrackedFiles, nothingToCommit := git.GetFilesOrNothingToCommit(status)
 
-	if len(selectedFiles) > 0 {
+	if nothingToCommit {
+		fmt.Println("Nothing to commit")
+		os.Exit(1)
+	}
 
-		printSelectedFiles(selectedFiles)
+	if len(untrackedFiles) > 0 {
+		resolveUntrackedFiles(untrackedFiles)
+	}
 
-		addFilesCommander := run.Commander{
-			Command: "git",
-			Args:    append([]string{"add"}, selectedFiles...),
-		}
+	notStaged := git.GetNotStagedFiles(status)
 
-		_, err := git.Add(addFilesCommander)
-
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			os.Exit(1)
-		}
+	if len(notStaged) > 0 {
+		stageFiles(notStaged)
 	}
 }
 
-func selectFilesToTrack(availableFiles []string) []string {
+func getStatus() string {
+	untrackedFilesCommander := run.Commander{
+		Command: "git",
+		Args:    []string{"status"},
+	}
 
+	status, err := git.Status(untrackedFilesCommander)
+
+	if err != nil {
+		fmt.Printf("error: %v", errors.WithMessage(err, ""))
+		os.Exit(1)
+	}
+
+	return status
+}
+
+func resolveUntrackedFiles(untrackedFiles []string) {
+	var selectedFiles []string
+
+	selectedFiles = askUserToSelectFiles(untrackedFiles, "You have untracked files. Select files to add.")
+
+	if len(selectedFiles) > 0 {
+		addFiles(selectedFiles)
+	}
+}
+
+func askUserToSelectFiles(availableFiles []string, message string) []string {
 	options := append([]string{"**Select all**", "**Exit and ignore selections**"}, availableFiles...)
 
 	result := []string{}
 	prompt := &survey.MultiSelect{
-		Message: "You have untracked files. Select files to add.",
+		Message: message,
 		Options: options,
 	}
 
@@ -107,7 +108,7 @@ func selectFilesToTrack(availableFiles []string) []string {
 }
 
 func stageFiles(availableFiles []string) {
-	selectedFiles := selectFilesToStage(availableFiles)
+	selectedFiles := askUserToSelectFiles(availableFiles, "You have unstaged files. Select files to add.")
 
 	if len(selectedFiles) > 0 {
 		var filesToAdd []string
@@ -116,6 +117,23 @@ func stageFiles(availableFiles []string) {
 			file = strings.Split(file, ":")[1]
 			filesToAdd = append(filesToAdd, strings.TrimSpace(file))
 		}
+
+		addFiles(filesToAdd)
+	}
+}
+
+func printSelectedFiles(files []string) {
+	fmt.Println("You selected:")
+
+	for _, file := range files {
+		fmt.Println(file)
+	}
+}
+
+func addFiles(filesToAdd []string) {
+	if len(filesToAdd) > 0 {
+
+		printSelectedFiles(filesToAdd)
 
 		addFilesCommander := run.Commander{
 			Command: "git",
@@ -128,37 +146,6 @@ func stageFiles(availableFiles []string) {
 			fmt.Printf("Prompt failed %v\n", err)
 			os.Exit(1)
 		}
-	}
-}
-
-func selectFilesToStage(availableFiles []string) []string {
-	options := append([]string{"**Select all**", "**Exit and ignore selections**"}, availableFiles...)
-
-	result := []string{}
-	prompt := &survey.MultiSelect{
-		Message: "You have unstaged files. Select files to add.",
-		Options: options,
-	}
-
-	survey.AskOne(prompt, &result, nil)
-
-	for i := 0; i < len(result); i++ {
-		if result[i] == "**Select all**" {
-			return availableFiles
-		}
-		if result[i] == "**Exit and ignore selections**" {
-			return nil
-		}
-	}
-
-	return result
-}
-
-func printSelectedFiles(files []string) {
-	fmt.Println("You selected:")
-
-	for _, file := range files {
-		fmt.Println(file)
 	}
 }
 
